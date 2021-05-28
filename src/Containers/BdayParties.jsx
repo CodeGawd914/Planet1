d74146ad7993b36288aa22a08d47084c7af196b0
denpatel <denpatel85@gmail.com>
2021-05-28 16:42:33 +0530

    Calendar Design Update

--- src/Containers/BdayParties.jsx
+++ src/Containers/BdayParties.jsx
@@ -40,7 +40,7 @@ const BdayParties = () => {
                         </List>
 
                             description=
-                            <List className="w3-left-align, KGN1">
+                            <List className="w3-center-align, KGN1">
                                 <List.Item>-90 Minute Private Party</List.Item>
                                 <List.Item>-$10 Arcade Playing Card Per Child</List.Item>
                                 <List.Item>-250 Bonus Tickets for the Birthday Star</List.Item>
@@ -68,7 +68,7 @@ const BdayParties = () => {
                         </List>
 
                             description=
-                            <List className="w3-left-align KGN1">
+                            <List className="w3-center-align KGN1">
                                 <List.Item>-90 Minute Private Party</List.Item>
                                 <List.Item>-$15 Arcade Playing Card Per Child</List.Item>
                                 <List.Item>-750 Bonus Tickets for the Birthday Star</List.Item>
